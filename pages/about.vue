5da5b884f191c64915d491d4672286b61de6da4d
Xolgrunek <xol@xolgoth.xyz>
2023-01-10 16:08:57 -0600

    init

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+  <h1 class="text-2xl text-red-600">About Zrekae</h1>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <NuxtLink to="/">Home page</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+  </main>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
+<style>
+  .red {
+    color:red;
+  }
+</style>
