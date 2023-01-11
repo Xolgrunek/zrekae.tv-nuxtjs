227af9bd24495b7a7ddc851b2e244248e8969e29
Xolgrunek <git@xolgoth.xyz>
2023-01-10 21:31:11 -0600

    learning experiments

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <NuxtLink to="/">Home page</NuxtLink>
+    <BaseButton class="px-6 py-2 bg-gray-500 font-medium text-sm hover:bg-gray-600 text-gray-100 rounded" to="/" rel="next">Home page</BaseButton>
     <NuxtLink to="/about">About</NuxtLink>
   </main>
 </template>
@@ -12,8 +12,3 @@ export default Vue.extend({
   name: 'IndexPage'
 })
 </script>
-<style>
-  .red {
-    color:red;
-  }
-</style>
---
+++ pages/partnerships.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+  <h1 class="text-center text-gray-500">Partnerships</h1>
+  <p>Hello, World!!!!</p>
+  <p>I am here!</p>
+  </div>
+</template>
